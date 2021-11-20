26da9430393250e373069134f3d0239bd04ffcfb
YuqinChen <yuqinche@usc.edu>
2021-11-19 16:19:07 -0800

    xgboost predict done

--- models_manager/xgboost_.py
+++ models_manager/xgboost_.py
@@ -1,62 +1,49 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
 import xgboost as xgb
 from sklearn import metrics
 import os
 
-<<<<<<< Updated upstream
 
-def train_and_get_metrics(path, feature_list=None):
-    if feature_list is None:
-        feature_list = ['Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
-                        'Color3', 'MaturitySize', 'FurLength', 'Vaccinated', 'Dewormed',
-                        'Sterilized', 'Health', 'Quantity', 'Fee', 'State']
-    df=pd.read_csv(path)
-    train, test=df[:14000], df[14000:]
-    train_y, test_y=train['AdoptionSpeed'], test['AdoptionSpeed']
-    train_x=train[feature_list]
-    test_x=test[feature_list]
-=======
-# print(1)
-# df=pd.read_csv('/Users/chenyuqin/Desktop/21_fall_codes_and_relative/dsci551/project/data/train/train.csv')
-# print(3)
-# train, test=df[:14000], df[14000:]
-# train_y, test_y=train['AdoptionSpeed'], test['AdoptionSpeed']
-# train_x=train[['Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
-#        'Color3', 'MaturitySize', 'FurLength', 'Vaccinated', 'Dewormed',
-#        'Sterilized', 'Health', 'Quantity', 'Fee', 'State']]
-# test_x=test[['Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
-#        'Color3', 'MaturitySize', 'FurLength', 'Vaccinated', 'Dewormed',
-#        'Sterilized', 'Health', 'Quantity', 'Fee', 'State']]
-# dtrain=xgb.DMatrix(train_x, label=train_y)
-# dtest=xgb.DMatrix(test_x, label=test_y)
-# model = xgb.train(params={}, dtrain=dtrain)
-# pred_y = model.predict(dtest)
-# accuracy = metrics.mean_squared_error(test_y,pred_y)
-# print(2)
-# print(accuracy)
+class MyXgboost:
+    def __init__(self):
+        pass
 
-def train_and_get_metrics(path):
-    df=pd.read_csv(path)
-    train, test=df[:14000], df[14000:]
-    train_y, test_y=train['AdoptionSpeed'], test['AdoptionSpeed']
-    train_x=train[['Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
-       'Color3', 'MaturitySize', 'FurLength', 'Vaccinated', 'Dewormed',
-       'Sterilized', 'Health', 'Quantity', 'Fee', 'State']]
-    test_x=test[['Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
-       'Color3', 'MaturitySize', 'FurLength', 'Vaccinated', 'Dewormed',
-       'Sterilized', 'Health', 'Quantity', 'Fee', 'State']]
->>>>>>> Stashed changes
-    dtrain=xgb.DMatrix(train_x, label=train_y)
-    dtest=xgb.DMatrix(test_x, label=test_y)
-    model = xgb.train(params={}, dtrain=dtrain)
-    pred_y = model.predict(dtest)
-    accuracy = metrics.mean_squared_error(test_y,pred_y)
-    return accuracy
+    def train_and_get_metrics(self, path=None, feature_list=None):
+        if feature_list is None:
+            feature_list = ['Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
+                            'Color3', 'MaturitySize', 'FurLength', 'Vaccinated', 'Dewormed',
+                            'Sterilized', 'Health', 'Quantity', 'Fee', 'State']
+            self.feature_list = feature_list
+        if path is None:
+            path = '/Users/chenyuqin/Desktop/21_fall_codes_and_relative/dsci551/project/data/train/train.csv'
+        df=pd.read_csv(path)
+        train, test=df[:14000], df[14000:]
+        train_y, test_y=train['AdoptionSpeed'], test['AdoptionSpeed']
+        train_x=train[feature_list]
+        # print(type(train_x), train_x)
+        test_x=test[feature_list]
+        dtrain=xgb.DMatrix(train_x, label=train_y)
+        dtest=xgb.DMatrix(test_x, label=test_y)
+        self.model = xgb.train(params={}, dtrain=dtrain)
+        pred_y = self.model.predict(dtest)
+        accuracy = metrics.mean_squared_error(test_y,pred_y)
+        return accuracy
+
+    def predict(self, path=None):
+        # path 是需要预测的文件，其中必须包含train时选择的特征。
+        if path is None:
+            path = '/Users/chenyuqin/Desktop/21_fall_codes_and_relative/dsci551/project/data/train/train.csv'
+            input = pd.read_csv(path)[self.feature_list]
+        dinput = xgb.DMatrix(input)
+        predict_days = self.model.predict(dinput)
+        return predict_days
 
 if __name__=='__main__':
-   # print(os.path.abspath('..'))
-#    print(train_and_get_metrics('../data/train/train.csv'))
-#    print(train_and_get_metrics('/Users/chenyuqin/Desktop/21_fall_codes_and_relative/dsci551/project/data/train/train.csv')
-   print(train_and_get_metrics('train.csv'))
+    # print(train_and_get_metrics('train.csv'))
+    myXgboost = MyXgboost()
+    print(myXgboost.train_and_get_metrics())
+    print(myXgboost.predict())
+
 
 
